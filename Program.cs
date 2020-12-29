9f8984bd0dfdb9199c1dacef105419d72da2f768
kamneklogs <kamneklogs@gmail.com>
2020-12-29 14:19:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace textInversorCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
